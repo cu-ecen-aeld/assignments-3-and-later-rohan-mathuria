33e22e7c960aff3364af12de5c46f1479adbe4f8
Rohan Mathuria <rohan.mathuria@aptiv.com>
2024-12-29 15:59:56 +0530

    Assignment 3 Part 1
    * implemented TODOs in syetemcalls.c

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,3 +1,10 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
 #include "systemcalls.h"
 
 /**
@@ -6,41 +13,56 @@
  *   successfully using the system() call, false if an error occurred,
  *   either in invocation of the system() call, or if a non-zero return
  *   value was returned by the command issued in @param cmd.
-*/
+ */
 bool do_system(const char *cmd)
 {
-
-/*
- * TODO  add your code here
- *  Call the system() function with the command set in the cmd
- *   and return a boolean true if the system() call completed with success
- *   or false() if it returned a failure
-*/
-
+    int ret;
+    ret = system(cmd);
+    if (ret == -1)
+    {
+        printf("Failed to create child process.\nError: %s\n", strerror(errno));
+        return false;
+    }
+    else if (WIFEXITED(ret) && WEXITSTATUS(ret) == 127)
+    {
+        printf("Shell could not be executed, invalid command. Status: %d\n", WEXITSTATUS(ret));
+        return false;
+    }
+    else if (WIFEXITED(ret) && WEXITSTATUS(ret) != 0)
+    {
+        printf("Command failed to run. Status: %d\nError: %s", WEXITSTATUS(ret), strerror(WEXITSTATUS(ret)));
+        return false;
+    }
+    else if (cmd == NULL)
+    {
+        if (ret == 0)
+            printf("cmd is NULL and shell is not available.\n");
+        else
+            printf("cmd is NULL and shell is available.\n");
+    }
     return true;
 }
 
 /**
-* @param count -The numbers of variables passed to the function. The variables are command to execute.
-*   followed by arguments to pass to the command
-*   Since exec() does not perform path expansion, the command to execute needs
-*   to be an absolute path.
-* @param ... - A list of 1 or more arguments after the @param count argument.
-*   The first is always the full path to the command to execute with execv()
-*   The remaining arguments are a list of arguments to pass to the command in execv()
-* @return true if the command @param ... with arguments @param arguments were executed successfully
-*   using the execv() call, false if an error occurred, either in invocation of the
-*   fork, waitpid, or execv() command, or if a non-zero return value was returned
-*   by the command issued in @param arguments with the specified arguments.
-*/
-
+ * @param count -The numbers of variables passed to the function. The variables are command to execute.
+ *   followed by arguments to pass to the command
+ *   Since exec() does not perform path expansion, the command to execute needs
+ *   to be an absolute path.
+ * @param ... - A list of 1 or more arguments after the @param count argument.
+ *   The first is always the full path to the command to execute with execv()
+ *   The remaining arguments are a list of arguments to pass to the command in execv()
+ * @return true if the command @param ... with arguments @param arguments were executed successfully
+ *   using the execv() call, false if an error occurred, either in invocation of the
+ *   fork, waitpid, or execv() command, or if a non-zero return value was returned
+ *   by the command issued in @param arguments with the specified arguments.
+ */
 bool do_exec(int count, ...)
 {
     va_list args;
     va_start(args, count);
-    char * command[count+1];
+    char *command[count + 1];
     int i;
-    for(i=0; i<count; i++)
+    for (i = 0; i < count; i++)
     {
         command[i] = va_arg(args, char *);
     }
@@ -49,33 +71,62 @@ bool do_exec(int count, ...)
     // and may be removed
     command[count] = command[count];
 
-/*
- * TODO:
- *   Execute a system command by calling fork, execv(),
- *   and wait instead of system (see LSP page 161).
- *   Use the command[0] as the full path to the command to execute
- *   (first argument to execv), and use the remaining arguments
- *   as second argument to the execv() command.
- *
-*/
+    for (i = 0; i < count; i++)
+    {
+        if (command[i][0] != '/' && command[i][0] != '-')
+        {
+            printf("%s is not an absolute path", command[i]);
+            return false;
+        }
+    }
 
+    fflush(stdout);
+    pid_t cpid;
+    cpid = fork();
+    if (cpid == -1)
+    {
+        printf("Failed to create child process.\nError: %s\n", strerror(errno));
+        return false;
+    }
+    if (cpid == 0)
+    { // child
+        int ret;
+        ret = execv(command[0], command);
+        if (ret == -1)
+        {
+            printf("execv failed.\nError: %s\n", strerror(errno));
+            return false;
+        }
+    }
+    else
+    { // parent
+        int wstatus;
+        pid_t wpid;
+        wpid = waitpid(cpid, &wstatus, 0);
+        if (wpid == -1)
+        {
+            printf("waitpid failed.\nError: %s\n", strerror(errno));
+            return false;
+        }
+        if (!WIFEXITED(wstatus) && !WEXITSTATUS(wstatus))
+            return false;
+    }
     va_end(args);
-
     return true;
 }
 
 /**
-* @param outputfile - The full path to the file to write with command output.
-*   This file will be closed at completion of the function call.
-* All other parameters, see do_exec above
-*/
+ * @param outputfile - The full path to the file to write with command output.
+ *   This file will be closed at completion of the function call.
+ * All other parameters, see do_exec above
+ */
 bool do_exec_redirect(const char *outputfile, int count, ...)
 {
     va_list args;
     va_start(args, count);
-    char * command[count+1];
+    char *command[count + 1];
     int i;
-    for(i=0; i<count; i++)
+    for (i = 0; i < count; i++)
     {
         command[i] = va_arg(args, char *);
     }
@@ -84,16 +135,53 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
     // and may be removed
     command[count] = command[count];
 
+    int fd;
+    fd = open(outputfile, O_WRONLY | O_TRUNC | O_CREAT, 0644);
+    if (fd < 0)
+    {
+        printf("Failed to open file");
+        return false;
+    }
 
-/*
- * TODO
- *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
- *   redirect standard out to a file specified by outputfile.
- *   The rest of the behaviour is same as do_exec()
- *
-*/
+    fflush(stdout);
+    pid_t cpid;
+    cpid = fork();
+    if (cpid == -1)
+    {
+        printf("Failed to create child process.\nError: %s\n", strerror(errno));
+        return false;
+    }
+    if (cpid == 0)
+    { // child
+        int ret;
+        if (dup2(fd, 1) < 0)
+        {
+            perror("dup2");
+            return false;
+        }
+        close(fd);
 
+        ret = execv(command[0], command);
+        if (ret == -1)
+        {
+            printf("execv failed.\nError: %s\n", strerror(errno));
+            return false;
+        }
+    }
+    else
+    { // parent
+        int wstatus;
+        pid_t wpid;
+        wpid = waitpid(cpid, &wstatus, 0);
+        if (wpid == -1)
+        {
+            printf("waitpid failed.\nError: %s\n", strerror(errno));
+            return false;
+        }
+        if (!WIFEXITED(wstatus) && !WEXITSTATUS(wstatus))
+            return false;
+        close(fd);
+    }
     va_end(args);
-
     return true;
 }
